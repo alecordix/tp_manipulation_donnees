54f0aa152c425fae0f8408b2ca71a7facf981e2d
alecordix <andrea.lecordix@gmail.com>
2020-04-22 10:57:37 +0200

    Json datas loaded

--- dossier_code/user_database.py
+++ dossier_code/user_database.py
@@ -1,6 +1,6 @@
 import json
-with open('users.json') as json_data:
-    print(type(json_data))
+# with open('users.json') as json_data:
+#     print(type(json_data))
 class UserDatabase:
     def __init__(self, users):
         self.users = users
@@ -9,8 +9,22 @@ class UserDatabase:
         datas = json.load(read_file)
         for a in datas:
             print(a.get("id"))
-            for l in d.get("champs2"):
-                print(l)
+        for b in datas:
+            print(b.get("nom"))
+        for c in datas:
+            print(c.get("prenom"))
+        for d in datas:
+            print(d.get("profession"))
+        for e in datas:
+            print(e.get("age"))
+        for f in datas:
+            print(f.get("sexe"))
+        for g in datas:
+            print(bool(g.get("marie")))
+        for h in datas:
+            print(h.get("hobbies"))
+
+
 class User:
     def __init__(self, id, age, gender, hobbies):
         self.id = id
