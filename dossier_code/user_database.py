e02aed36994474ea345be1370922a3a42c4fefdf
alecordix <andrea.lecordix@gmail.com>
2020-04-24 17:01:54 +0200

    Méthodes terminées. Place aux tests unitaires !

--- dossier_code/user_database.py
+++ dossier_code/user_database.py
@@ -9,52 +9,26 @@ Déclaration des classes
 class UserDatabase:
 
     def __init__(self, users_list):
-        """
-        Attributs de la classe UserDatabase:
-        J'ai renommé users en users_list pour que le nom soit plus parlant
-            * users_list  : une liste d'objets de type User
-                ==> si on est à l'intérieur de la classe : on accède à cette variable avec self.users_list
-                ==> si on est à l'extérieur de la classe :
-                    on accède à cette variable à partir d'une variable de type UserDatabase préalablement créée
-                    exemple:
-                         database = UserDatabase()  # je crée une variable de type UserDatabase
-                         database.users_list        # j'accède à la liste de cette database
-        """
         self.users_list = users_list
 
     def display(self):
-        # J'ai ajouté cette méthode pour vous permettre d'afficher le contenu de UserDatabase
-        # (contenu = les utilisateurs présents dans la base)
         print("Utilisateurs présents dans la base:")
         for u in self.users_list:
             print("{ id:", u.id, ", age:", u.age, ", sexe:", u.sexe, ", hobbies:", u.hobbies, "}")
 
     def display_age(self):
-        # J'ai ajouté cette méthode pour vous permettre d'afficher le contenu de UserDatabase
-        # (contenu = les utilisateurs présents dans la base)
         print("L'age des utilisateurs présents dans la base:")
         for u in self.users_list:
             print("age:", u.age)
 
-    # def get_nb_users_old(self):
-    #     # TODO: à supprimer ==> je laisse le code ici pour vous faire quelques retours
-    #     users = 0
-    #     for users in users.json : # ATTENTION à votre nommage: ici users est l'entier déclaré à la ligne précédente (donc il n'a pas d'attribut json)
-    #         users += 1
-    #     print("Le nombre de users est", {self.id})  # le mot-clé self permet d'accéder aux attributs de la classe dans laquelle on se trouve (self.id ne fonctionne que si on est à l'intérieur de la classe User)
-    #     # retourner le nombre d'utilisateurs
-    #     # (sinon ce ne sera pas possible de faire un test unitaire sur cette méthode)
-    #     return users
-
     def get_nb_users(self):
         print()
-        # j'ai apporté les corrections nécessaires pour que votre code fonctionne
-        users_list = self.users_list  # liste des utilisateurs présents dans la base
+        users_list = self.users_list
         users = 0
         for u in users_list:
             users += 1
         print("Le nombre de users est")
-        return users  # je retourne le nombre d'utilisateurs
+        return users
 
     def cal_average(self):
 
@@ -93,10 +67,9 @@ class UserDatabase:
 
     def get_random_users(self):
         import random
-        import collections
         tot_ids = []
         x = random.randint(1,len(self.users_list))
-        print(x)
+        print("Le chiffre aléatoire est :", x)
         for u in self.users_list:
             tot_ids.append(u.id)
         result= random.sample(tot_ids, x)
@@ -108,35 +81,6 @@ class UserDatabase:
             users_json = json.load(read_file)
             for user in users_json:
                 self.users_list.append(User(user.get("id"), user.get("age"), user.get("sexe"), user.get("hobbies")))
-
-    # with open('users.json', 'r') as read_file:
-    #      datas = json.load(read_file)
-    #      for a in datas:
-    #          print(a.get("id"))
-    #      for b in datas:
-    #          print(b.get("nom"))
-    #      for c in datas:
-    #          print(c.get("prenom"))
-    #      for d in datas:
-    # #
-    # print(d.get("profession"))
-    # #
-    # for e in datas:
-    # #
-    # print(e.get("age"))
-    # #
-    # for f in datas:
-    # #
-    # print(f.get("sexe"))
-    # #
-    # for g in datas:
-    # #
-    # print(bool(g.get("marie")))
-    # #
-    # for h in datas:
-    #
-    # print(h.get("hobbies"))
-
 
 class User:
     def __init__(self, id, age, sexe, hobbies):
@@ -185,4 +129,4 @@ print("Les id des hommes de 25 à 30 ans inclus et qui aiment le cinema sont:", 
 # selection
 print()
 random_users = database.get_random_users()
-print(random_users)
+print("Liste de users aléatoires :", random_users)
